e5ff4f10f739cfc9d0b936e95859735946c49a23
ankurraj <ankur.raj@xpressbees.com>
2023-10-12 00:00:50 +0530

    webcam added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import React, { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import addNotification, { Notifications } from "react-push-notification";
-
-import { fetchWeather } from "./api/fetchWeather";
 import "./App.css";
+import Profile from "./Profile";
+import Weather from "./Weather";
 
 const App = () => {
-  const [query, setQuery] = useState("");
-  const [weather, setWeather] = useState({});
-
-  const search = async (e) => {
-    if (e.key === "Enter") {
-      const data = await fetchWeather(query);
-
-      setWeather(data);
-      setQuery("");
-    }
-  };
-
   //   const showNotification = () => {
   //     console.log("first");
   //     Notification.requestPermission().then((perm) => {
@@ -41,37 +31,15 @@ const App = () => {
   return (
     <>
       <Notifications />
-      <div className="main-container">
-        <input
-          type="text"
-          className="search"
-          placeholder="Search..."
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          onKeyPress={search}
-        />
-        {weather.main && (
-          <div className="city">
-            <h2 className="city-name">
-              <span>{weather.name}</span>
-              <sup>{weather.sys.country}</sup>
-            </h2>
-            <div className="city-temp">
-              {Math.round(weather.main.temp)}
-              <sup>&deg;C</sup>
-            </div>
-            <div className="info">
-              <img
-                className="city-icon"
-                src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-                alt={weather.weather[0].description}
-              />
-              <p>{weather.weather[0].description}</p>
-            </div>
-          </div>
-        )}
-        <button onClick={showNotification}>Show Notification</button>
-      </div>
+      {/* <Weather />
+      <button onClick={showNotification}>Show Notification</button>
+      <Profile /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Weather />} />
+          <Route path="/webcam" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
