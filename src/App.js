d1884d7e9994029298dcc70d3d396b245f609be7
ankurraj <ankur.raj@xpressbees.com>
2023-10-11 23:27:25 +0530

    push notification

--- src/App.js
+++ src/App.js
@@ -1,42 +1,79 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import addNotification, { Notifications } from "react-push-notification";
 
-import { fetchWeather } from './api/fetchWeather';
-import './App.css';
+import { fetchWeather } from "./api/fetchWeather";
+import "./App.css";
 
 const App = () => {
-    const [query, setQuery] = useState('');
-    const [weather, setWeather] = useState({});
-    
-    const search = async (e) => {
-        if(e.key === 'Enter') {
-            const data = await fetchWeather(query);
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
 
-            setWeather(data);
-            setQuery('');
-        }
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchWeather(query);
+
+      setWeather(data);
+      setQuery("");
     }
+  };
 
-    return (
-        <div className="main-container">
-            <input type="text"className="search"placeholder="Search..."value={query}onChange={(e) => setQuery(e.target.value)}onKeyPress={search}/>
-            {weather.main && (
-                <div className="city">
-                    <h2 className="city-name">
-                        <span>{weather.name}</span>
-                        <sup>{weather.sys.country}</sup>
-                    </h2>
-                    <div className="city-temp">
-                        {Math.round(weather.main.temp)}
-                        <sup>&deg;C</sup>
-                    </div>
-                    <div className="info">
-                        <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
-                        <p>{weather.weather[0].description}</p>
-                    </div>
-                </div>
-            )}
-        </div>
-    );
-}
+  //   const showNotification = () => {
+  //     console.log("first");
+  //     Notification.requestPermission().then((perm) => {
+  //       if (perm === "granted") {
+  //         new Notification("example notification");
+  //       }
+  //     });
+  //   };
+
+  const showNotification = () => {
+    console.log("first");
+    addNotification({
+      title: "Push notification",
+      subtitle: "demo",
+      message: "Dummy notification",
+      theme: "lightblue",
+      position: "top-left",
+      //   native: true,
+    });
+  };
+
+  return (
+    <>
+      <Notifications />
+      <div className="main-container">
+        <input
+          type="text"
+          className="search"
+          placeholder="Search..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          onKeyPress={search}
+        />
+        {weather.main && (
+          <div className="city">
+            <h2 className="city-name">
+              <span>{weather.name}</span>
+              <sup>{weather.sys.country}</sup>
+            </h2>
+            <div className="city-temp">
+              {Math.round(weather.main.temp)}
+              <sup>&deg;C</sup>
+            </div>
+            <div className="info">
+              <img
+                className="city-icon"
+                src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                alt={weather.weather[0].description}
+              />
+              <p>{weather.weather[0].description}</p>
+            </div>
+          </div>
+        )}
+        <button onClick={showNotification}>Show Notification</button>
+      </div>
+    </>
+  );
+};
 
 export default App;
