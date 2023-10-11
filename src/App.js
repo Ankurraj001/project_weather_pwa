b57a657d692a079ac88b1d4306c39c93e6ffc015
ankurraj <ankur.raj@xpressbees.com>
2023-10-12 00:04:05 +0530

    webcam code

--- src/App.js
+++ src/App.js
@@ -7,33 +7,9 @@ import Profile from "./Profile";
 import Weather from "./Weather";
 
 const App = () => {
-  //   const showNotification = () => {
-  //     console.log("first");
-  //     Notification.requestPermission().then((perm) => {
-  //       if (perm === "granted") {
-  //         new Notification("example notification");
-  //       }
-  //     });
-  //   };
-
-  const showNotification = () => {
-    console.log("first");
-    addNotification({
-      title: "Push notification",
-      subtitle: "demo",
-      message: "Dummy notification",
-      theme: "lightblue",
-      position: "top-left",
-      //   native: true,
-    });
-  };
-
   return (
     <>
       <Notifications />
-      {/* <Weather />
-      <button onClick={showNotification}>Show Notification</button>
-      <Profile /> */}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Weather />} />
